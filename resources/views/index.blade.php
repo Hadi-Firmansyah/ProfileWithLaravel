b224a72aae8fa6b7fd6138b634baf25d9d236738
Hadi Firmansyah <hadifirmansyah.id@gmail.com>
2020-07-22 13:03:57 +0700

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <title>Laravel Pemula</title>
+  </head>
+  <body style="font-family : 'Quicksand';">
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-danger">
+      <div class="container" >
+      <a class="navbar-brand" href="#">Hadi Firmansyah</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Profile<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portofolio">Portofolio</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+      </nav>
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+        <img src="img/unnamed.png" width="200px" class="rounded-circle">
+          <h1 class="display-4">Hadi Firmansyah</h1>
+            <a class="lead">
+            <hr>
+            Student | Programmer | Blogger | Youtuber
+            </a>
+            <hr>
+            <a>
+            Smk Taruna Bhakti Depok - Hadifirmansyah.id@gmail.com
+            </a>
+            
+      </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center">
+      <h3 id="experience">Experience</h3>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col text-center">
+    Nama Saya Hadi Firmansyah Dari SMK TARUNA BHAKTI DEPOK - Selama Saya Sekolah Saya Sudah Bisa Sedikit Memahami
+    Apa Itu Pemrograman Sehingga Membuat Saya Mempunyai Beberapa Pengalaman Seperti Bagaimana Proses Pembuatan Website,
+    Aplikasi Android, Aplikasi Dekstop DLL.
+    Dengan Ilmu Saya Yang Sedikit Ini Semoga Kedepannya Saya Bisa Menambah Pengalaman Yang Baik Dan Dapat Berguna Bagi Saya
+    Dan Orang Lain.
+    <br><br><br>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center">
+      <h3 id="portofolio">Portofolio</h3>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+        <div class="card">
+          <img src="img/dicoding.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Mengikuti Kelas Dicoding Pemrograman Web Pemula</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+          <img src="img/piket.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Membuat Web Manajemen Piket Kelas</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+          <img src="img/dekstop.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Membuat Aplikasi Penjualan Berbasis Dekstop</p>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+      
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
